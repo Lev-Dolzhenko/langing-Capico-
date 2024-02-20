e8810f008c6f045a2a26911f28f271f0e7ace293
Leo <workspace2707@yandex.ru>
2024-02-20 11:05:19 +0300

    created manage block

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+//header
+
 const infoButton = document.querySelector(".info__button");
 
 const infoButtonCircle = document.querySelector(".info__button_circle");
@@ -21,4 +23,47 @@ infoButton.addEventListener("mouseover", () => {
 infoButton.addEventListener("mouseleave", () => {
   infoButtonCircle.classList.remove("circleAnimationHeaderButtom");
   infoButtonCircle.classList.add("circleAnimationHeaderReverse");
+});
+
+//manage
+
+const videoManage = document.querySelector(".manage__video");
+const buttonManage = document.querySelector(".manage__button");
+const manageFrame = document.querySelector(".manage__frame");
+const overlayVideo = document.querySelector(".overlay_video");
+
+buttonManage.addEventListener("click", function (event) {
+  event.stopPropagation();
+  manageFrame.classList.remove("manageFrameAnimationReverse");
+  overlayVideo.classList.remove("overlayVideoReverse");
+  manageFrame.classList.add("manageFrameAnimation");
+  overlayVideo.classList.add("overlayVideo");
+});
+
+const handleClickOutside = (event) => {
+  if (manageFrame.classList.contains("manageFrameAnimation")) {
+    if (
+      manageFrame &&
+      !manageFrame.contains(event.target) &&
+      event.target !== buttonManage
+    ) {
+      manageFrame.classList.remove("manageFrameAnimation");
+      overlayVideo.classList.remove("overlayVideo");
+      manageFrame.classList.add("manageFrameAnimationReverse");
+      overlayVideo.classList.add("overlayVideoReverse");
+      console.log("not elem");
+    }
+  }
+};
+
+document.addEventListener("click", handleClickOutside);
+
+buttonManage.addEventListener("mouseover", () => {
+  videoManage.classList.remove("manageVideoAnimationReverse");
+  videoManage.classList.add("manageVideoAnimation");
+});
+
+buttonManage.addEventListener("mouseleave", () => {
+  videoManage.classList.remove("manageVideoAnimation");
+  videoManage.classList.add("manageVideoAnimationReverse");
 });
